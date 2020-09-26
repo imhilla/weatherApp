b431049053f98e7c137019daf171bbd6fae687c7
imhilla <hillaryodhiambo282@gmail.com>
2020-09-26 11:23:40 +0300

    refactor preload

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ import weather from './weather'
 import { getLocation } from './weather';
 import giphy from './giphy'
 
+var myVar;
 
+function preLoad() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
