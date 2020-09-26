235f04fe64f05503522b0d372033304df31d8d77
imhilla <hillaryodhiambo282@gmail.com>
2020-09-26 11:42:46 +0300

    refactor preloaders

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import weather from './weather'
 import { getLocation } from './weather';
 import giphy from './giphy'
 
-var myVar;
 
-function preLoad() {
-  myVar = setTimeout(showPage, 3000);
-}
+const divLoad = document.getElementById('onLoad')
+divLoad.addEventListener('load', preLoad)
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+var myVar;
+function preLoad() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+preLoad()
