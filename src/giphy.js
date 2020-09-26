f7a25b508d71e2318b8d686e68217bbf7bb59481
imhilla <hillaryodhiambo282@gmail.com>
2020-09-26 16:52:31 +0300

    add error handling to giphy

--- src/giphy.js
+++ src/giphy.js
@@ -1,13 +1,17 @@
 const giphy = (() => {
   const img = document.querySelector('img');
   async function getCats() {
-    let store = JSON.parse(localStorage.getItem('desc'));
-    let type = store[store.length-1]
-    let lasttype = type.split(' ')
-    let last = lasttype[lasttype.length-1]
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=ucKwEuUMMg7PY1Qdv2AGIokrGfc0zbGH&s=${last}`, { mode: 'cors' });
-    const catData = await response.json();
-    img.src = catData.data.images.original.url;
+    try {
+      let store = JSON.parse(localStorage.getItem('desc'));
+      let type = store[store.length - 1]
+      let lasttype = type.split(' ')
+      let last = lasttype[lasttype.length - 1]
+      const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=ucKwEuUMMg7PY1Qdv2AGIokrGfc0zbGH&s=${last}`, { mode: 'cors' });
+      const catData = await response.json();
+      img.src = catData.data.images.original.url;
+    } catch (err) {
+       alert(err);
+    }
   }
 
   getCats()
